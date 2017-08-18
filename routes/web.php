5a4808540f875544249ba938bee98facca660ed2
Kumanan <kumani@gmail.com>
2017-08-18 19:15:29 +0530

    my first commit

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('contact', function () {
+    return view('contact');
+});
 
 Route::get('/', function () {
     return view('welcome');
